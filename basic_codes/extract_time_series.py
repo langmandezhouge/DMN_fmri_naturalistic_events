53cc8ee40e0a452755e7f82afaf4a3e333bf7e61
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-08-28 07:37:41 +0800

    Update and rename extract_timepoint_series.py to extract_time_series.py

--- basic_codes/extract_time_series.py
+++ basic_codes/extract_time_series.py
@@ -2,25 +2,26 @@ import nibabel as nib
 import numpy as np
 import os
 
-data = 
+input_dir = '/prot/lkz/searchlight/Naturalistic/results/data.nii.gz'
+data = nib.load(inputdim_dir)
 
 '''# way 1:
 # Gain mask
 from nilearn import masking
-mask = masking.compute_background_mask(data_i)
+mask = masking.compute_background_mask(data)
 # Gain the TimeSeries
 from nilearn.input_data import NiftiMasker,  MultiNiftiMasker
 masker = NiftiMasker(mask_img=mask, standardize=True)
-time_series = masker.fit_transform(data_i)'''
+time_series = masker.fit_transform(data)'''
 
 # way 2:
 from nilearn import masking
-mask = masking.compute_background_mask(data_i)
+mask = masking.compute_background_mask(data)
 print(mask.get_data().shape)
 from nilearn.masking import apply_mask
-time_series_02 = apply_mask(data_i, mask)
+time_series = apply_mask(data, mask)
 
-print(time_series_02.shape)
+print(time_series.shape)
 #print(time_series)
 #save the time series(numpy)
-np.save(os.path.join(output_dir, 'time_series_02'),time_series_02)
+np.save(os.path.join(output_dir, 'time_series'),time_series)
