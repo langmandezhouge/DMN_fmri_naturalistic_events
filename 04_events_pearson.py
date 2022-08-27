45caf5f900d62722f53b135663f7bc6aa5587ac6
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-08-27 17:23:12 +0800

    Update 04_events_pearson.py

--- 04_events_pearson.py
+++ 04_events_pearson.py
@@ -7,10 +7,8 @@ import pandas as pd
 from scipy.stats import pearsonr
 from scipy.stats import stats
 import os
-
-input_dir = '/prot/lkz/searchlihgt_pearson/pearson/results/event_result/0.npy'
-dataset = np.load(input_dir,allow_pickle=True)
-print(dataset.shape)
+import seaborn as sb
+import matplotlib.pyplot as plt
 
 #方式一：两个向量之间做相关
 '''pearson = np.corrcoef(x, y)  #x和y为两个向量.若为矩阵，则先将x和y拉成一维向量，再做相关
@@ -19,20 +17,37 @@ print(pearson)'''
 
 
 #方式二：一个矩阵内列与列之间做相关（即构建事件关系矩阵）
-data = np.transpose(dataset) # 将得到的每一个体素的所有事件数组转置为（27，m），列为事件数m
-print(data.shape)
 
-#pearson correlation analysis
-data = pd.DataFrame(data)
-pearson = data.corr() #对每个体素的所有事件数组，列与列之间做相关
-#data.corr(method='spearman') #other method
-#data.corr(method='kendall')
-print(pearson.shape)
-print(pearson)
+path = '/prot/lkz/searchlihgt_pearson/pearson/results/voxel_events_result/'
 
-#将相关矩阵转化为热力图
-import seaborn as sb
-import matplotlib.pyplot as plt
-sb.heatmap(data = pearson)
-#sb.heatmap(data = pearson,cmap="YlGnBu") # cmap为不同的颜色背景
-plt.show()
+files = os.listdir(path)
+files.sort(key=lambda x:int(x[:-4]))
+print(files)
+
+count = len(files)
+print(count)
+
+for i in range(count):
+    datas = files[i]
+    dataset = np.load(path + datas)
+    print(dataset.shape)
+    data = np.transpose(dataset) # zhuanzhi
+    print(data.shape)
+    print(data)
+    data = pd.DataFrame(data)
+
+    pearson = data.corr()
+    print(pearson.shape)
+    print(pearson)
+    #data.corr(method='spearman') #other method
+    #data.corr(method='kendall')
+
+    output_dir = '/prot/lkz/searchlihgt_pearson/pearson/results/events_pearson_corr_result/'
+    np.save(os.path.join(output_dir, str(i)), pearson)
+
+    heatmap = sb.heatmap(pearson)
+    #sb.heatmap(data = pearson,cmap="YlGnBu")
+    plt.show()
+
+    heatmap = heatmap.get_figure()
+    heatmap.savefig(os.path.join(output_dir, str(i)), dpi = 300)
