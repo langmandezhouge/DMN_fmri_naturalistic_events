316cd2808ea7a819f4c2b33c5a024f34852e348e
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-09-01 02:51:14 +0800

    Update isc_mean.py

--- analysis_method/isc_mean.py
+++ analysis_method/isc_mean.py
@@ -1,3 +1,5 @@
+'''Compute ISC'''
+
 import warnings
 import sys
 if not sys.warnoptions:
@@ -21,137 +23,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style = 'white', context='talk', font_scale=1, rc={"lines.linewidth": 2})
 
+
+# way 1
 path = '/prot/lkz/Pieman2_isc/Pieman2/'
 
 dir_mask = os.path.join(path, 'masks/')
 mask_name = os.path.join(dir_mask, 'avg152T1_gray_3mm.nii.gz')
 all_task_names = ['word', 'intact1']
-all_task_des = ['word level scramble', 'intact story']
-n_subjs_total = 18
-group_assignment_dict = {task_name: i for i, task_name in enumerate(all_task_names)}
-
-results = '/prot/lkz/Pieman2_isc/Pieman2/results/'
-# Where do you want to store the data
-dir_out = results + 'isc/'
-if not os.path.exists(dir_out):
-    os.makedirs(dir_out)
-    print('Dir %s created ' % dir_out)
-
-
-def get_file_names(data_dir_, task_name_, verbose=False):
-    """
-    Get all the participant file names
-
-    Parameters
-    ----------
-    data_dir_ [str]: the data root dir
-    task_name_ [str]: the name of the task
-
-    Return
-    ----------
-    fnames_ [list]: file names for all subjs
-    """
-    c_ = 0
-    fnames_ = []
-    # Collect all file names
-    for subj in range(1, n_subjs_total+1):
-        fname = os.path.join(
-            data_dir_, 'sub-%.3d/func/sub-%.3d-task-%s.nii.gz' % (subj, subj, task_name_))
-
-        # If the file exists
-        if os.path.exists(fname):
-
-            # Add to the list of file names
-            fnames_.append(fname)
-
-    return fnames_
-
 
 """load brain template"""
 # Load the brain mask
 brain_mask = io.load_boolean_mask(mask_name)
-
 # Get the list of nonzero voxel coordinates
 coords = np.where(brain_mask)
-
 # Load the brain nii image
 brain_nii = nib.load(mask_name)
 
+# Custom mean estimator with Fisher z transformation for correlations
+def compute_summary_statistic(iscs, summary_statistic='mean', axis=None):
+    return np.tanh(np.nanmean(np.arctanh(iscs), axis=axis))
 
-"""load bold data"""
-# load the functional data
-fnames = {}
-images = {}
-masked_images = {}
-bold = {}
-group_assignment = []
-n_subjs = {}
+task_isc = np.load('/prot/lkz/Pieman2_isc/Pieman2/results/isc/isc_maps_word.npy')
 
-for task_name in all_task_names:
-    data_path = '/prot/lkz/Pieman2_isc/Pieman2/'
-    fnames[task_name] = get_file_names(data_path, task_name)
-    images[task_name] = io.load_images(fnames[task_name])
-    masked_images[task_name] = image.mask_images(images[task_name], brain_mask)
-    # Concatenate all of the masked images across participants
-    bold[task_name] = image.MaskedMultiSubjectData.from_masked_images(
-        masked_images[task_name], len(fnames[task_name])
-    )
-    # Convert nans into zeros
-    bold[task_name][np.isnan(bold[task_name])] = 0
-    # compute the group assignment label
-    n_subjs_this_task = np.shape(bold[task_name])[-1]
-    group_assignment += list(
-        np.repeat(group_assignment_dict[task_name], n_subjs_this_task)
-    )
-    n_subjs[task_name] = np.shape(bold[task_name])[-1]
-    print('Data loaded: {} \t shape: {}' .format(task_name, np.shape(bold[task_name])))
+# Fisher Z-transformed mean across subjects
+task_mean = compute_summary_statistic(task_isc, axis=0) # mean isc
+print(task_mean.shape)
 
 
-'''Compute ISC'''
+# way 2
 # run ISC, loop over conditions
-isc_maps = {}
-for task_name in all_task_names:
-    isc_maps[task_name] = isc(bold[task_name], pairwise=False,summary_statistic='mean')
-    
-    
-    # Make the ISC output a volume
-    isc_vol = np.zeros(brain_nii.shape)
-    # Map the ISC data for the first participant into brain space
-    isc_vol[coords] = isc_maps[task_name]
-    # make a nii image of the isc map
-    isc_nifti = nib.Nifti1Image(isc_vol, brain_nii.affine, brain_nii.header)
-
-    # Plot the data as a statmap
-    threshold = .2
-
-    f, ax = plt.subplots(1, 1, figsize=(12, 5))
-    plotting.plot_stat_map(
-        isc_nifti,
-        threshold=threshold,
-        axes=ax
-    )
-    ax.set_title('ISC map for subjects mean, task = {}'.format(task_name))
-    plt.show()
-
-    '''volumetric space to surface space'''
-
-    view = 'medial'
-
-    # get a surface
-    fsaverage = datasets.fetch_surf_fsaverage()
-
-    # make "texture"
-    texture = surface.vol_to_surf(isc_nifti, fsaverage.pial_left)
-
-    # plot
-    title_text = ('Left Avg ISC map, {} for sub-mean'.format(task_name))
-    surf_left_map = plotting.plot_surf_stat_map(
-        fsaverage.infl_left, texture,
-        hemi='left', view=view,
-        title=title_text,
-        threshold=threshold, cmap='RdYlBu_r',
-        colorbar=True,
-        bg_map=fsaverage.sulc_left)
-    #plt.savefig(os.path.join(subj_dir + 'fsaverage_left_ISC_%s_sub-%.3d.png' % (task_name, subj_id + 1)),
-               # bbox_inches='tight', dpi=450)
-    plt.show()
+#def isc(data, pairwise=False, summary_statistic=mean, tolerate_nans=True)
