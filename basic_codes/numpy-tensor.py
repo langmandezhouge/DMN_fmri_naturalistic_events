649b376eb5534827b4a39a04597a2c0ca751a721
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-10-05 17:14:42 +0800

    Update numpy-tensor.py

--- basic_codes/numpy-tensor.py
+++ basic_codes/numpy-tensor.py
@@ -1,1 +1,17 @@
 
+import numpy as np
+import torch
+
+x = np.ones((2,2))
+print(x)
+print(type(x))
+
+x = torch.tensor(x)
+#x = torch.from_numpy(x)
+print(x)
+
+# tensor-npy
+x_= x.numpy()
+#x_= x.detach().numpy()
+print(x_)
+print(type(x_))
