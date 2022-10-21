ef6e141ef275b359de03b6f4576a0af0d5d5974a
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-10-21 16:06:53 +0800

    Update and rename mask_roi.py to roi_mask.py

--- roi_bold/roi_mask.py
+++ roi_bold/roi_mask.py
@@ -2,6 +2,7 @@
 import numpy as np
 import nibabel as nib
 import os
+from nilearn.input_data import NiftiLabelsMasker
 
 basePath = '/prot/lkz/brain_roi_mask/'
 atlasName = "atlas_246.nii"
@@ -11,14 +12,14 @@ atlas_nii = nib.load(atlasFile)
 atlas_arr = atlas_nii.get_fdata()
 
 mask_arr = atlas_arr.copy()
-for i in range(10):
+for i in range(246):
     roiIndex = i +1
     mask_arr[atlas_arr != roiIndex] = 0
     mask_arr[atlas_arr == roiIndex] = 1
     mask_affine = atlas_nii.affine.copy()
     mask_hrd = atlas_nii.header.copy()
     mask_hrd["cal_max"] = 1
-
-    output = '/prot/lkz/LSTM/results/mask/'
+    # print(mask_hrd)
+    output = '/prot/lkz/LSTM/roi_results/roi_mask/'
     mask_nii = nib.Nifti1Image(mask_arr, mask_affine, mask_hrd)
     nib.save(mask_nii, os.path.join(output, "roi_" + str('%03d'%roiIndex) + ".nii.gz"))
