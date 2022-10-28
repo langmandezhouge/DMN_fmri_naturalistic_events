6f88159e5a9f01abd859f82d0b3212fabc59bc71
langmandezhouge <46048467+langmandezhouge@users.noreply.github.com>
2022-10-28 15:49:54 +0800

    Create events-vector_times_aligning.py

---
+++ myproject/myproject_gist/events-vector_times_aligning.py
@@ -0,0 +1,23 @@
+import pickle
+import os
+import numpy as np
+
+path = '/prot/lkz/LSTM/text-gist/text_gist-results/'
+files = '/prot/lkz/LSTM/text-gist/text_gist-results/stimuli/'
+
+for i in os.listdir(files):
+    time_path = path + "events_times/" + i + ".txt"
+    event_path = path + "vector_result/" + i + ".txt"
+    time_file = open(time_path, "rb")
+    time_file = pickle.load(time_file)
+    num = len(time_file)
+
+    event_file = open(event_path, "rb")
+    event_file = pickle.load(event_file)
+    final_event_file = event_file[:num]
+    print(final_event_file.shape)
+
+    output = path + "events-vector_times_aligning/"
+    if not os.path.exists(output):
+        os.makedirs(output)
+    np.save(os.path.join(output, i), final_event_file)
